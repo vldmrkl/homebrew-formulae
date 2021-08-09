0e040d3d37232bd27321d202a64d08d33a46fa9a
Volodymyr <klymenkodp@gmail.com>
2021-08-08 20:38:39 -0400

    Update airdrop-cli sha

--- Formula/airdrop-cli.rb
+++ Formula/airdrop-cli.rb
@@ -2,7 +2,7 @@ class AirdropCli < Formula
   desc "macOS CLI for AirDrop"
   homepage "https://github.com/vldmrkl/airdrop-cli"
   url "https://github.com/vldmrkl/airdrop-cli/archive/1.0.1.tar.gz"
-  sha256 "3749d9f308e8651b8220ae09eab4917743f1f89d"
+  sha256 "2e93796692fddb13e362bfd9c204bfc2f24ef22fadaf09efe97201d0bc9b54c3"
   license "MIT"
   head "https://github.com/vldmrkl/airdrop-cli.git"
   version "1.0.1"
