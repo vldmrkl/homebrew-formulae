df6978625c93c4d5775d4e447774c0b67958fa21
Volodymyr <klymenkodp@gmail.com>
2021-08-08 20:34:11 -0400

    Update airdrop-cli 1.0.0 -> 1.0.1

--- Formula/airdrop-cli.rb
+++ Formula/airdrop-cli.rb
@@ -1,11 +1,11 @@
 class AirdropCli < Formula
   desc "macOS CLI for AirDrop"
   homepage "https://github.com/vldmrkl/airdrop-cli"
-  url "https://github.com/vldmrkl/airdrop-cli/archive/1.0.0.tar.gz"
-  sha256 "525139ebb848a88db661bfda4bc685ba9cb8c3ba92801c7666f5590e6525beee"
+  url "https://github.com/vldmrkl/airdrop-cli/archive/1.0.1.tar.gz"
+  sha256 "3749d9f308e8651b8220ae09eab4917743f1f89d"
   license "MIT"
   head "https://github.com/vldmrkl/airdrop-cli.git"
-
+  version "1.0.1"
   depends_on xcode: "11.4"
 
   def install
